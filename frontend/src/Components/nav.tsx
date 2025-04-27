33f4882cf82158c2dbe90325d52d69bf4cb9089b
aravinnndddd <aromalaravind0@gmail.com>
2025-04-27 14:41:24 +0530

    Update nav.tsx

--- frontend/src/Components/nav.tsx
+++ frontend/src/Components/nav.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Menu } from 'lucide-react';
+import { Menu,X } from 'lucide-react';
 
 const Nav: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
